bcf988f1e1acd7a42798568aa057f88a94f7593f
albertgalicinskiy <galicinskiyalba@mail.ru>
2022-05-10 20:24:30 +0500

    I have reconfigured the HW5 for using map(object) instead of second provider

--- HW5_done/create_rg_storacc_blobcont/providers.tf
+++ HW5_done/create_rg_storacc_blobcont/providers.tf
@@ -1,11 +1,7 @@
 #-----------------------------------------------------------------------
 # Providers configuration
 #-----------------------------------------------------------------------
-provider "azurerm" {
-  features {}
-}
 
 provider "azurerm" {
   features {}
-  alias = "provider2"
 }
